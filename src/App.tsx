8e5fe5f57319d383f5d2ce59044c3d446e3474c8
zinminthet <cvnxsolution@gmail.com>
2025-04-18 12:17:02 +0630

    build a colormodeswitch

--- src/App.tsx
+++ src/App.tsx
@@ -12,6 +12,7 @@ const App = () => {
     >
       <GridItem area={"nav"}>
         <NavBar />
+        
       </GridItem>
       <Show above="lg">
         <GridItem area={"aside"}>aside</GridItem>
