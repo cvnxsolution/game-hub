d30d36e0c10ecc092bcee61768633b0cbeca6206
zinminthet <cvnxsolution@gmail.com>
2025-04-18 16:48:51 +0630

    show a skeleton while fetchting genres

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -2,26 +2,40 @@ import React from "react";
 import useGenres from "../hooks/useGenres";
 import useData from "../hooks/useData";
 import { Genre } from "../hooks/useGenres";
-import { HStack, Image, List, ListItem, Text } from "@chakra-ui/react";
+import {
+  HStack,
+  Image,
+  List,
+  ListItem,
+  Skeleton,
+  Spinner,
+  Text,
+} from "@chakra-ui/react";
 import getCroppedImageUrl from "../services/image-url";
+import GenreSkelton from "./GenreSkelton";
+import GenreSkeleton from "./GenreSkelton";
 
 const GenreList = () => {
   const { data, error, isLoading } = useGenres();
+  const skeltons = [1, 2, 3, 123, 12, 3, 12312, 3];
   return (
-    <List>
-      {data.map((g) => (
-        <ListItem key={g.id} paddingY="5px">
-          <HStack>
-            <Image
-              boxSize="32px"
-              borderRadius={8}
-              src={getCroppedImageUrl(g.image_background)}
-            />
-            <Text fontSize={"large"}>{g.name}</Text>
-          </HStack>
-        </ListItem>
-      ))}
-    </List>
+    <>
+      {isLoading && <GenreSkeleton skeletons={skeltons} />}
+      <List>
+        {data.map((g) => (
+          <ListItem key={g.id} paddingY="5px">
+            <HStack>
+              <Image
+                boxSize="32px"
+                borderRadius={8}
+                src={getCroppedImageUrl(g.image_background)}
+              />
+              <Text fontSize={"large"}>{g.name}</Text>
+            </HStack>
+          </ListItem>
+        ))}
+      </List>
+    </>
   );
 };
 
