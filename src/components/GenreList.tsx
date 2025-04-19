09723d26760e4e7a52b3580539c8355ba54b96ad
zinminthet <cvnxsolution@gmail.com>
2025-04-19 20:45:00 +0630

    added description sample"

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -16,7 +16,11 @@ import getCroppedImageUrl from "../services/image-url";
 import GenreSkelton from "./GenreSkelton";
 import GenreSkeleton from "./GenreSkelton";
 
-const GenreList = () => {
+interface Props {
+  onSelectGenre: (genre: Genre) => void;
+}
+
+const GenreList = ({ onSelectGenre }: Props) => {
   const { data, error, isLoading } = useGenres();
   const skeltons = [1, 2, 3, 123, 12, 3, 12312, 3];
   return (
@@ -30,8 +34,13 @@ const GenreList = () => {
                 boxSize="32px"
                 borderRadius={8}
                 src={getCroppedImageUrl(g.image_background)}
+                objectFit={"cover"}
               />
-              <Button variant={"link"} fontSize={"large"}>
+              <Button
+                variant={"link"}
+                fontSize={"large"}
+                onClick={() => onSelectGenre()}
+              >
                 {g.name}
               </Button>
             </HStack>
